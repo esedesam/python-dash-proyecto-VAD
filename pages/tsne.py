623f40286a4fd2c2432f88f5d09d09cc5f168b5a
jemarle <jesusmartinezleal2001@gmail.com>
2024-02-26 13:46:24 +0100

    Añadir botones para cambiar hiperparámetros en TSNE.

--- pages/tsne.py
+++ pages/tsne.py
@@ -1,11 +1,10 @@
 import dash
-from dash import dcc, html
+from dash import dcc, html, Input, Output, callback,  State
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 import numpy as np
-from dash import Dash, dcc, html, Input, Output, callback
+from dash.exceptions import PreventUpdate
 from sklearn.manifold import TSNE
 from io import StringIO
 import sys
@@ -14,15 +13,12 @@ dash.register_page(__name__)
 
 rs = 0
 n_components = 2
-perplexity = 30
 n_iter = 10000
-learning_rate = 200
 n_iter_without_progress = 300
 verbose = 1
 method = 'exact'
 init = 'pca'
 early_exaggeration = 25
-
 
 # Definir el layout de la aplicación
 layout = dbc.Container([
@@ -42,6 +38,13 @@ layout = dbc.Container([
             'backgroundColor': 'rgb(250, 250, 250)',
             'padding': '5px 10px'}),
     html.Div([
+    html.Label('Perplexity:'),
+    dcc.Input(id='perplexity-input', type='number', value=30, min=5, max=50, step=1),
+    html.Label('Learning Rate:'),
+    dcc.Input(id='learning-rate-input', type='number', value=200, min=10, max=1000, step=10),
+    html.Button('Update', id='update-button', n_clicks=0)
+    ], style={'margin-top': '20px'}),
+    html.Div([
         dcc.Graph(
             id='crossfilter-indicator-scatter2',
             hoverData={'points': [{'hovertext': 'P1'}]})],
@@ -50,17 +53,16 @@ layout = dbc.Container([
         dcc.Graph(id='x-hist2'),
         dcc.Graph(id='y-hist2')],
         style={'display': 'inline-block', 'width': '49%', 'padding': '5px 5px'})
-    ],
+],
     fluid=True
 )
 
-# Interacción de store-data -> rehacer dropdowns
 @callback(
-        Output('crossfilter-xaxis-column2', 'options'),
-        Output('crossfilter-xaxis-column2', 'value'),
-        Output('crossfilter-yaxis-column2', 'options'),
-        Output('crossfilter-yaxis-column2', 'value'),
-        Input('store', 'data'))
+    Output('crossfilter-xaxis-column2', 'options'),
+    Output('crossfilter-xaxis-column2', 'value'),
+    Output('crossfilter-yaxis-column2', 'options'),
+    Output('crossfilter-yaxis-column2', 'value'),
+    Input('store', 'data'))
 def updateColDropdown(storeData):
     if 'data' in storeData and not storeData['data'] == {}:
         newOptions = list(storeData['data'].keys())[1:]
@@ -72,28 +74,49 @@ def updateColDropdown(storeData):
     Output('crossfilter-indicator-scatter2', 'figure'),
     Input('crossfilter-xaxis-column2', 'value'),
     Input('crossfilter-yaxis-column2', 'value'),
-    Input('store', 'data'))
-
-def update_graph(xaxis_column_name, yaxis_column_name, storeData):
-    if 'data' in storeData and not storeData['data'] == {}:
-        df = pd.DataFrame.from_dict(storeData['data'])
-        df_tsne = perform_tsne(df)
-        df_tsne['AV'] = df_tsne['AV'].astype(str) # para poner discreta la leyenda
-        
-        fig = px.scatter(
-            df_tsne,  # Usamos la proyección t-SNE en lugar de los datos originales
-            x='t-SNE Component 1',  # Nombre de la primera dimensión después de la proyección
-            y='t-SNE Component 2',  # Nombre de la segunda dimensión después de la proyección
-            height=500,
-            template="ggplot2",
-            hover_name=df[df.columns[0]],  
-            color='AV') # revisar si sale lo de tsne en el mismo orden que está en el dataframe
+    Input('store', 'data'),
+    Input('update-button', 'n_clicks'),
+    State('perplexity-input', 'value'),
+    State('learning-rate-input', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name, storeData, n_clicks, perplexity_value, learning_rate_value):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if 'update-button' in changed_id:
+        if n_clicks > 0:
+            return generate_tsne_scatter(storeData, perplexity_value, learning_rate_value)
+        else:
+            raise PreventUpdate
     else:
-        fig = px.scatter()
+        if 'data' in storeData and not storeData['data'] == {}:
+            return generate_tsne_scatter(storeData)
+        else:
+            return px.scatter()
+
+def generate_tsne_scatter(storeData, perplexity_value=30, learning_rate_value=200):
+    df = pd.DataFrame.from_dict(storeData['data'])
+    df_tsne = perform_tsne(df, perplexity_value, learning_rate_value)
+    df_tsne['AV'] = df_tsne['AV'].astype(str)
+    
+    fig = px.scatter(
+        df_tsne,
+        x='t-SNE Component 1',
+        y='t-SNE Component 2',
+        height=500,
+        template="ggplot2",
+        hover_name=df[df.columns[0]],
+        color='AV')
     return fig
 
+def perform_tsne(df, perplexity_value=30, learning_rate_value=200):
+    tsne = TSNE(n_components=n_components, random_state=rs, perplexity=perplexity_value, n_iter=n_iter,
+                learning_rate=learning_rate_value, verbose=verbose,
+                n_iter_without_progress=n_iter_without_progress, method=method, init=init,
+                early_exaggeration=early_exaggeration)
 
-# Función para crear histograma
+    df_dropped = df.drop([df.columns[0], df.columns[-1]], axis=1)
+    tsne_result = tsne.fit_transform(df_dropped)
+    tsne_df = pd.DataFrame(tsne_result, columns=['t-SNE Component 1', 't-SNE Component 2'])
+    tsne_df['AV'] = df['AV'].values
+    return tsne_df
 
 def create_hist(df, xaxis_column_name, nBins=10, patient=None, patientsColName='PACIENTES'):
     binValues, binEdges = np.histogram(df[xaxis_column_name].values, bins=nBins)
@@ -126,18 +149,18 @@ def create_hist(df, xaxis_column_name, nBins=10, patient=None, patientsColName='
             line={'width': 1},
             fillcolor='red',
             opacity=1)
-        
     fig.update_layout(
         margin={'l': 5, 'r': 5, 't': 20, 'b': 5})
     return fig
 
-# Interacción de hoverData o menús -> rehacer series 
 @callback(
     Output('x-hist2', 'figure'),
     Input('crossfilter-indicator-scatter2', 'hoverData'),
     Input('crossfilter-xaxis-column2', 'value'),
     Input('store', 'data'))
 def update_x_hist(hoverData, xaxis_column_name, storeData):
+    if hoverData is None:
+        raise PreventUpdate
     patient = hoverData['points'][0]['hovertext']
     df = pd.DataFrame.from_dict(storeData['data'])
     fig = create_hist(df, xaxis_column_name, patient=patient)
@@ -149,19 +172,9 @@ def update_x_hist(hoverData, xaxis_column_name, storeData):
     Input('crossfilter-yaxis-column2', 'value'),
     Input('store', 'data'))
 def update_y_hist(hoverData, yaxis_column_name, storeData):
+    if hoverData is None:
+        raise PreventUpdate
     patient = hoverData['points'][0]['hovertext']
     df = pd.DataFrame.from_dict(storeData['data'])
     fig = create_hist(df, yaxis_column_name, patient=patient)
     return fig
-
-def perform_tsne(df):
-
-    tsne = TSNE(n_components = n_components, random_state = rs, perplexity = perplexity, n_iter = n_iter, learning_rate = learning_rate, verbose = verbose,  
-                n_iter_without_progress=n_iter_without_progress, method = method, init = init, early_exaggeration = early_exaggeration)
-    
-    df_dropped = df.drop([df.columns[0], df.columns[-1]], axis=1)
-    tsne_result = tsne.fit_transform(df_dropped)
-    tsne_df = pd.DataFrame(tsne_result, columns=['t-SNE Component 1', 't-SNE Component 2'])
-    tsne_df['AV'] = df['AV'].values # para asignar vector y no otro dataframe
-    
-    return tsne_df
